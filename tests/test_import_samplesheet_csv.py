632e7c28f93ee3d5eec7b12de8d48f930d98c196
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-09-27 14:06:12 +0200

    add test for duplicate readgroup

--- tests/test_import_samplesheet_csv.py
+++ tests/test_import_samplesheet_csv.py
@@ -20,51 +20,58 @@
 
 from pathlib import Path
 
-from biowdl_input_converter import input_conversions, output_conversions
+from biowdl_input_converter.input_conversions import \
+    samplesheet_csv_to_samplegroup
+from biowdl_input_converter.output_conversions import \
+    samplegroup_to_biowdl_old_yaml
 
 import pytest
 
-filesdir = Path(__file__).parent / Path("files")
+FILESDIR = Path(__file__).parent / Path("files")
 
 
 def test_main_comma_complete():
-    with (Path(filesdir) / Path("complete.yml")).open() as f:
+    with (FILESDIR / Path("complete.yml")).open() as f:
         result = f.read()
-    samplegroup = input_conversions.samplesheet_csv_to_samplegroup(
-        Path(filesdir) / Path("complete.csv"))
-    assert output_conversions.samplegroup_to_biowdl_old_yaml(
-        samplegroup) == result
+    samplegroup = samplesheet_csv_to_samplegroup(
+        FILESDIR / Path("complete.csv"))
+    assert samplegroup_to_biowdl_old_yaml(samplegroup) == result
 
 
 def test_main_semicolon_without_md5():
-    with (Path(filesdir) / Path("without_md5.yml")).open() as f:
+    with (FILESDIR / Path("without_md5.yml")).open() as f:
         result = f.read()
-    samplegroup = input_conversions.samplesheet_csv_to_samplegroup(
-        Path(filesdir) / Path("without_md5.csv"))
-    assert output_conversions.samplegroup_to_biowdl_old_yaml(
-        samplegroup) == result
+    samplegroup = samplesheet_csv_to_samplegroup(
+        FILESDIR / Path("without_md5.csv"))
+    assert samplegroup_to_biowdl_old_yaml(samplegroup) == result
 
 
 def test_main_tab_without_readgroup():
-    with (Path(filesdir) / Path("without_readgroup.yml")).open() as f:
+    with (FILESDIR / Path("without_readgroup.yml")).open() as f:
         result = f.read()
-    samplegroup = input_conversions.samplesheet_csv_to_samplegroup(
-        Path(filesdir) / Path("without_readgroup.tsv"))
-    assert output_conversions.samplegroup_to_biowdl_old_yaml(
-        samplegroup) == result
+    samplegroup = samplesheet_csv_to_samplegroup(
+        FILESDIR / Path("without_readgroup.tsv"))
+    assert samplegroup_to_biowdl_old_yaml(samplegroup) == result
 
 
 def test_main_missing_field():
     with pytest.raises(KeyError) as e:
-        input_conversions.samplesheet_csv_to_samplegroup(
-            Path(filesdir) / Path("missing_field.csv"))
-    e.match("sample")
+        samplesheet_csv_to_samplegroup(
+            FILESDIR / Path("missing_field.csv"))
+    assert e.match("sample")
 
 
 def test_extra_field():
-    samplesheet = input_conversions.samplesheet_csv_to_samplegroup(
-        Path(filesdir) / Path("extra_fields.csv"))
+    samplesheet = samplesheet_csv_to_samplegroup(
+        FILESDIR / Path("extra_fields.csv"))
     assert samplesheet[0].additional_properties["extra_field1"] == "xf1"
     assert samplesheet[0].additional_properties["extra_field2"] == "xf2"
     assert samplesheet[1].additional_properties["extra_field1"] == "xfI"
     assert samplesheet[1].additional_properties["extra_field2"] == "xfII"
+
+
+def test_duplicate_readgroup():
+    with pytest.raises(ValueError) as error:
+        samplesheet_csv_to_samplegroup(
+            FILESDIR / Path("duplicate_readgroup.csv"))
+    assert error.match("Duplicate readgroup id s2-lib1-rg1")
