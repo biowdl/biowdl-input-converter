61bab2910e3f5c430950b953955e160d8812f6fe
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-11-20 13:37:31 +0100

    stable version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ LONG_DESCRIPTION = Path("README.rst").read_text()
 
 setup(
     name="biowdl-input-converter",
-    version="0.3.0-dev",
+    version="0.2.1",
     description="Converting various input formats into WDL structs for BioWDL "
                 "pipelines.",
     author="Leiden University Medical Center",
